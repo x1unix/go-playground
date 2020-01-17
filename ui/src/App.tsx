5c1148a488c044677654ff433944ad86f4033a88
x1unix <denis0051@gmail.com>
2020-01-18 01:30:06 +0200

    ui: add theming for fabric

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { loadTheme } from '@uifabric/styling';
 
 import { store } from './store';
 import { Header } from './Header';
+import { LightTheme, DarkTheme } from './services/colors';
 import CodeEditor from './editor/CodeEditor';
 import './App.css';
 import Preview from './Preview';
 
-// import { loadTheme } from '@uifabric/styling';
-
-// loadTheme({
-//     palette:
-// });
+const changeFabricTheme = () => {
+    const { darkMode } = store.getState();
+    loadTheme(darkMode ? DarkTheme : LightTheme);
+};
+store.subscribe(changeFabricTheme);
+changeFabricTheme();
 
 function App() {
   return (
