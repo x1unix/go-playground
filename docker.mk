c2f31585fec25eceea9bcf3ed4bf11d5f28f38a8
x1unix <denis0051@gmail.com>
2020-01-27 07:28:04 +0200

    docker: pass version and clean interval

--- docker.mk
+++ docker.mk
@@ -23,4 +23,4 @@ docker-make-image:
 		echo "required parameter TAG is undefined" && exit 1; \
 	fi;
 	@echo "- Building '$(IMG_NAME):latest' $(TAG)..."
-	docker image build -t $(IMG_NAME):latest -t $(IMG_NAME):$(TAG) -f $(DOCKERFILE) .
+	docker image build -t $(IMG_NAME):latest -t $(IMG_NAME):$(TAG) -f $(DOCKERFILE) --build-arg APP_VERSION=$(TAG) .
