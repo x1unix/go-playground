6260eb3163a01ae77614b81beb870ce504342c32
Denis Sedchenko <denis0051@gmail.com>
2021-08-10 23:09:40 -0400

    Refactor makefile (#71)
    * Update build.mk
    
    * Update docker.mk
    
    * Update build.mk
    
    * Update build.mk
    
    * Update build.mk

--- build.mk
+++ build.mk
@@ -6,37 +6,38 @@ WEBWORKER_PKG ?= ./cmd/webworker
 
 .PHONY: clean
 clean:
-	rm -rf $(TARGET) && rm -rf $(UI)/build
+	@echo ":: Cleanup..." && rm -rf $(TARGET) && rm -rf $(UI)/build
 
 # Build targets
 .PHONY: collect-meta
 collect-meta:
-	@$(GO) run $(TOOLS)/collector -goroot $(GOROOT) -out data/packages.json
+	@echo ":: Building Go packages index..." && \
+	$(GO) run $(TOOLS)/collector -goroot $(GOROOT) -out data/packages.json
 
 .PHONY:preinstall
 preinstall:
-	@echo "- Installing dependencies..."
+	@echo ":: Checking and installing dependencies..." && \
 	cd $(UI) && yarn install --silent
 
 .PHONY:build-server
 build-server:
-	@echo "- Building server..."
+	@echo ":: Building server..." && \
 	go build -o $(TARGET)/playground $(PKG)
 
 .PHONY:build-ui
 build-ui:
-	@echo "- Building UI..."
+	@echo ":: Building UI..." && \
 	cd $(UI) && yarn build
 
 .PHONY:build-webworker
 build-webworker:
-	@echo "Building Go Webworker module..." && \
+	@echo ":: Building Go Webworker module..." && \
 	GOOS=js GOARCH=wasm $(GO) build -o $(PUBLIC_DIR)/worker.wasm $(WEBWORKER_PKG) && \
 	cp "$(GOROOT)/misc/wasm/wasm_exec.js" $(PUBLIC_DIR)
 
 .PHONY: build
 build: clean preinstall collect-meta build-server build-webworker build-ui
-	@echo "- Copying assets..."
-	cp -rf ./data $(TARGET)/data
-	mv $(UI)/build $(TARGET)/public
-	@echo "DONE!"
+	@echo ":: Copying assets..." && \
+	cp -rfv ./data $(TARGET)/data && \
+	mv -v $(UI)/build $(TARGET)/public && \
+	echo ":: Build done - $(TARGET)"
--- docker.mk
+++ docker.mk
@@ -3,14 +3,14 @@ IMG_NAME ?= x1unix/go-playground
 
 .PHONY: docker
 docker: docker-image docker-push-image
-	@echo "[✓] Done"
+	@echo ":: [✓] Done"
 
 .PHONY: docker-push-image
 docker-push-image: docker-login
 	@if [ -z "$(TAG)" ]; then\
 		echo "required parameter TAG is undefined" && exit 1; \
 	fi;
-	@echo "- Pushing $(IMG_NAME):$(TAG) (as latest)..."
+	@echo ":: Pushing $(IMG_NAME):$(TAG) (as latest)..."
 	docker push $(IMG_NAME):$(TAG)
 	docker push $(IMG_NAME):latest
 
@@ -29,7 +29,7 @@ docker-image:
 	@if [ -z "$(TAG)" ]; then\
 		echo "required parameter TAG is undefined" && exit 1; \
 	fi;
-	@echo "- Building '$(IMG_NAME):latest' $(TAG)..."
+	@echo ":: Building '$(IMG_NAME):latest' $(TAG)..."
 	docker image build -t $(IMG_NAME):latest -t $(IMG_NAME):$(TAG) -f $(DOCKERFILE) \
 		--build-arg APP_VERSION=$(TAG) \
 		--build-arg APP_GTAG=$(GTAG) .
