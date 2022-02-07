8db826e1f675e36dbf6d96432090b51d02be5dc2
Denis Sedchenko <denis0051@gmail.com>
2022-02-07 18:59:25 +0200

    Web: add ESLint (#90)
    * web: add eslint
    
    * web: fix type definition for NodeCallback
    
    * web: remove unused import in go bridge
    
    * ci: run ESLint on merge request
    
    * ci: fix working directory
    
    * ci: cache Go modules and build cache

--- web/src/services/go/foundation.ts
+++ web/src/services/go/foundation.ts
@@ -3,18 +3,18 @@ export type int = number;
 export type byte = number;
 
 export type bytes = Array<number>;
-export type NodeCallback<T> = (Error, T) => void;
+export type NodeCallback<T> = (err: Error | null | undefined, result?: T | null) => void;
 
 export const encoder = new TextEncoder();
 export const decoder = new TextDecoder('utf-8');
 
 
 export class SyscallError extends Error {
-    constructor(public code: string, message: string) {
-        super(message);
-    }
+  constructor(public code: string, message: string) {
+    super(message);
+  }
 }
 
 export const enosys = () => {
-    return new SyscallError('ENOSYS', 'not implemented')
+  return new SyscallError('ENOSYS', 'not implemented')
 };
