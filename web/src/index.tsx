a26ef9c337280644ccf4c72063eb9db6e02f9fb1
x1unix <denis0051@gmail.com>
2022-02-08 17:24:30 +0200

    web: update webworker code from recent react-scripts

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { initializeIcons } from '@fluentui/react/lib/Icons';
 import { registerGoLanguageProvider } from '~/components/editor/provider';
 import apiClient from '~/services/api';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import App from './App';
 import './index.css';
 
@@ -15,5 +15,5 @@ ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
