171d0b658f5840b9bee9c82c6bb43e4ead5efcb4
x1unix <denis0051@gmail.com>
2022-02-08 11:50:50 +0200

    web: reformat CodeEditor.tsx

--- web/src/components/editor/CodeEditor.tsx
+++ web/src/components/editor/CodeEditor.tsx
@@ -10,97 +10,97 @@ import { LANGUAGE_GOLANG, stateToOptions } from './props';
 const ANALYZE_DEBOUNCE_TIME = 500;
 
 interface CodeEditorState {
-    code?: string
-    loading?: boolean
+  code?: string
+  loading?: boolean
 }
 
 @Connect(s => ({
-    code: s.editor.code,
-    darkMode: s.settings.darkMode,
-    loading: s.status?.loading,
-    options: s.monaco,
+  code: s.editor.code,
+  darkMode: s.settings.darkMode,
+  loading: s.status?.loading,
+  options: s.monaco,
 }))
 export default class CodeEditor extends React.Component<any, CodeEditorState> {
-    analyzer?: Analyzer;
-    _previousTimeout: any;
-    editorInstance?: editor.IStandaloneCodeEditor;
+  analyzer?: Analyzer;
+  _previousTimeout: any;
+  editorInstance?: editor.IStandaloneCodeEditor;
 
-    editorDidMount(editorInstance: editor.IStandaloneCodeEditor, _: monaco.editor.IEditorConstructionOptions) {
-        this.editorInstance = editorInstance;
-        if (Analyzer.supported()) {
-            this.analyzer = new Analyzer();
-        } else {
-            console.info('Analyzer requires WebAssembly support');
+  editorDidMount(editorInstance: editor.IStandaloneCodeEditor, _: monaco.editor.IEditorConstructionOptions) {
+    this.editorInstance = editorInstance;
+    if (Analyzer.supported()) {
+      this.analyzer = new Analyzer();
+    } else {
+      console.info('Analyzer requires WebAssembly support');
+    }
+
+    const actions = [
+      {
+        id: 'run-code',
+        label: 'Build And Run Code',
+        contextMenuGroupId: 'navigation',
+        keybindings: [
+          monaco.KeyMod.CtrlCmd | monaco.KeyCode.Enter
+        ],
+        run: (ed, ...args) => {
+          this.props.dispatch(runFileDispatcher);
         }
-
-        const actions = [
-            {
-                id: 'run-code',
-                label: 'Build And Run Code',
-                contextMenuGroupId: 'navigation',
-                keybindings: [
-                    monaco.KeyMod.CtrlCmd | monaco.KeyCode.Enter
-                ],
-                run: (ed, ...args) => {
-                    this.props.dispatch(runFileDispatcher);
-                }
-            },
-            {
-                id: 'format-code',
-                label: 'Format Code (goimports)',
-                contextMenuGroupId: 'navigation',
-                keybindings: [
-                    monaco.KeyMod.CtrlCmd | monaco.KeyMod.Shift | monaco.KeyCode.KeyF
-                ],
-                run: (ed, ...args) => {
-                    this.props.dispatch(formatFileDispatcher);
-                }
-            }
-        ];
-
-        // Register custom actions
-        actions.forEach(action => editorInstance.addAction(action))
-        editorInstance.focus();
-    }
-
-    componentWillUnmount() {
-        this.analyzer?.dispose();
-    }
-
-    onChange(newValue: string, e: editor.IModelContentChangedEvent) {
-        this.props.dispatch(newFileChangeAction(newValue));
-
-        if (this.analyzer) {
-            this.doAnalyze(newValue);
+      },
+      {
+        id: 'format-code',
+        label: 'Format Code (goimports)',
+        contextMenuGroupId: 'navigation',
+        keybindings: [
+          monaco.KeyMod.CtrlCmd | monaco.KeyMod.Shift | monaco.KeyCode.KeyF
+        ],
+        run: (ed, ...args) => {
+          this.props.dispatch(formatFileDispatcher);
         }
+      }
+    ];
+
+    // Register custom actions
+    actions.forEach(action => editorInstance.addAction(action))
+    editorInstance.focus();
+  }
+
+  componentWillUnmount() {
+    this.analyzer?.dispose();
+  }
+
+  onChange(newValue: string, e: editor.IModelContentChangedEvent) {
+    this.props.dispatch(newFileChangeAction(newValue));
+
+    if (this.analyzer) {
+      this.doAnalyze(newValue);
+    }
+  }
+
+  private doAnalyze(code: string) {
+    if (this._previousTimeout) {
+      clearTimeout(this._previousTimeout);
     }
 
-    private doAnalyze(code: string) {
-        if (this._previousTimeout) {
-            clearTimeout(this._previousTimeout);
-        }
+    this._previousTimeout = setTimeout(() => {
+      this._previousTimeout = null;
+      this.analyzer?.analyzeCode(code).then(result => {
+        editor.setModelMarkers(
+          this.editorInstance?.getModel() as editor.ITextModel,
+          this.editorInstance?.getId() as string,
+          result.markers
+        );
+      }).catch(err => console.error('failed to perform code analysis: %s', err));
+    }, ANALYZE_DEBOUNCE_TIME);
+  }
 
-        this._previousTimeout = setTimeout(() => {
-            this._previousTimeout = null;
-            this.analyzer?.analyzeCode(code).then(result => {
-                editor.setModelMarkers(
-                    this.editorInstance?.getModel() as editor.ITextModel,
-                    this.editorInstance?.getId() as string,
-                    result.markers
-                );
-            }).catch(err => console.error('failed to perform code analysis: %s', err));
-        }, ANALYZE_DEBOUNCE_TIME);
-    }
-
-    render() {
-        const options = stateToOptions(this.props.options);
-        return <MonacoEditor
-            language={LANGUAGE_GOLANG}
-            theme={this.props.darkMode ? 'vs-dark' : 'vs-light'}
-            value={this.props.code}
-            options={options}
-            onChange={(newVal, e) => this.onChange(newVal, e)}
-            editorDidMount={(e, m: any) => this.editorDidMount(e, m)}
-        />;
-    }
+  render() {
+    const options = stateToOptions(this.props.options);
+    return <MonacoEditor
+      language={LANGUAGE_GOLANG}
+      theme={this.props.darkMode ? 'vs-dark' : 'vs-light'}
+      value={this.props.code}
+      options={options}
+      onChange={(newVal, e) => this.onChange(newVal, e)}
+      editorDidMount={(e, m: any) => this.editorDidMount(e, m)}
+    />;
+  }
 }
