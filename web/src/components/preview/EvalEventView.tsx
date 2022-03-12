2f09269ed21e0cef564942b378d2bbb14c266341
Denis Sedchenko <denis0051@gmail.com>
2022-03-12 02:18:30 +0100

    Add images output support (#115)
    * web: add image output support
    
    * web: add custom icon support for snippets
    
    * web: add image output snippets

--- web/src/components/preview/EvalEventView.tsx
+++ web/src/components/preview/EvalEventView.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './EvalEventView.css';
 
+const imageSectionPrefix = 'IMAGE:';
+const base64RegEx = /^[A-Za-z0-9+/]+[=]{0,2}$/;
 const nanosec = 1000000;
 
 interface ViewData {
@@ -11,6 +13,15 @@ interface ViewData {
 }
 
 const pad = (num: number, size: number) => ('000000000' + num).substr(-size);
+
+const isImageLine = (message: string) => {
+  if (!message?.startsWith(imageSectionPrefix)) {
+    return [false, null];
+  }
+
+  const payload = message.substring(imageSectionPrefix.length).trim();
+  return [base64RegEx.test(payload), payload];
+};
 
 export default class EvalEventView extends React.Component<ViewData> {
   get delay() {
@@ -23,9 +34,15 @@ export default class EvalEventView extends React.Component<ViewData> {
   }
 
   render() {
+    const { message, showDelay } = this.props;
+    const [isImage, payload] = isImageLine(message);
     return <div className="evalEvent">
-      <pre className={this.domClassName}>{this.props.message}</pre>
-      <span className="evalEvent__delay">{this.props.showDelay ? `[${this.delay}]` : ''}</span>
+      { isImage ? (
+        <img src={`data:image;base64,${payload}`} alt=""/>
+      ) : (
+        <pre className={this.domClassName}>{message}</pre>
+      )}
+      <span className="evalEvent__delay">{showDelay ? `[${this.delay}]` : ''}</span>
     </div>
   }
 }
