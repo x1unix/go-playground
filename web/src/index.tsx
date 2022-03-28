8e68e5f22b3f454a3423f5dbf0fb79c7e1647b86
x1unix <denis0051@gmail.com>
2022-03-28 03:21:22 +0200

    web: check service-worker for updates

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -16,4 +16,9 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.register({
+  onUpdate: (registration) => {
+    registration.update()
+      .catch(err => console.error('Failed to check updates: ', err))
+  }
+});
