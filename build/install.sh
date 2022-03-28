41292a630f9d2d8c5853aac3eefc57235db57dfb
x1unix <denis0051@gmail.com>
2022-03-28 04:22:51 +0200

    etc: add install script

---
+++ build/install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+set -e
+
+SVC_NAME='better-go-playground'
+SVC_USER='go-playground'
+DST_DIR='/opt/go-playground'
+
+echo ":: Creating a user '$SVC_USER'..."
+useradd -m --system go-playground
+
+echo ":: Copying files..."
+cp -rfv ./target "$DST_DIR"
+chown -R -v "$SVC_USER:$SVC_USER" "$DST_DIR"
+chmod +x "$DST_DIR/playground"
+
+echo ":: Installing service..."
+cp -fv "./build/$SVC_NAME.service" /etc/systemd/system
+systemctl daemon reload
+
+echo ":: Starting service..."
+systemctl enable --now "$SVC_NAME"
+
+echo "Installation finished. Use 'systemctl start|stop|restart|status $SVC_NAME' to start, stop or restart the service."
