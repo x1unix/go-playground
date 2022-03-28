363ece432be6febdb40641f75c396d585727a8af
x1unix <denis0051@gmail.com>
2022-03-28 03:51:50 +0200

    etc: check if yarn and Go are installed before build

--- build.mk
+++ build.mk
@@ -1,4 +1,5 @@
 GO ?= go
+YARN ?= yarn
 GOROOT ?= $(shell go env GOROOT)
 TOOLS ?= ./tools
 PUBLIC_DIR ?= $(UI)/public
@@ -7,6 +8,20 @@ WEBWORKER_PKG ?= ./cmd/webworker
 .PHONY: clean
 clean:
 	@echo ":: Cleanup..." && rm -rf $(TARGET) && rm -rf $(UI)/build
+
+.PHONY:check-go
+check-go:
+	@if ! command -v $(GO) >/dev/null 2>&1 ; then\
+		echo "ERROR: '$(GO)' binary not found. Please ensure that Go is installed or specify binary path with 'GO' variable." && \
+		exit 1; \
+	fi;
+
+.PHONY:check-yarn
+check-yarn:
+	@if ! command -v $(YARN) >/dev/null 2>&1 ; then\
+		echo "ERROR: '$(YARN)' binary not found. Please ensure that Node.js and Yarn are installed or specify binary path with 'YARN' variable." && \
+		exit 1; \
+	fi;
 
 # Build targets
 .PHONY: collect-meta
@@ -17,17 +32,17 @@ collect-meta:
 .PHONY:preinstall
 preinstall:
 	@echo ":: Checking and installing dependencies..." && \
-	cd $(UI) && yarn install --silent
+	cd $(UI) && $(YARN) install --silent
 
 .PHONY:build-server
 build-server:
 	@echo ":: Building server..." && \
-	go build -o $(TARGET)/playground $(PKG)
+	$(GO) build -o $(TARGET)/playground $(PKG)
 
 .PHONY:build-ui
 build-ui:
 	@echo ":: Building UI..." && \
-	cd $(UI) && yarn build
+	cd $(UI) && $(YARN) build
 
 .PHONY:build-webworker
 build-webworker:
@@ -36,7 +51,7 @@ build-webworker:
 	cp "$(GOROOT)/misc/wasm/wasm_exec.js" $(PUBLIC_DIR)
 
 .PHONY: build
-build: clean preinstall collect-meta build-server build-webworker build-ui
+build: check-go check-yarn clean preinstall collect-meta build-server build-webworker build-ui
 	@echo ":: Copying assets..." && \
 	cp -rfv ./data $(TARGET)/data && \
 	mv -v $(UI)/build $(TARGET)/public && \
