d70b0187d183ff3c322c20ac4073197b4550938a
x1unix <denis0051@gmail.com>
2022-05-30 01:44:52 +0200

    feat(ui): add icons for environment names

--- web/src/components/modals/EnvironmentSelectModal/EnvironmentSelectModal.tsx
+++ web/src/components/modals/EnvironmentSelectModal/EnvironmentSelectModal.tsx
@@ -4,24 +4,30 @@ import { Modal } from '@fluentui/react/lib/Modal';
 import {
   Stack, IStackTokens, IconButton, mergeStyleSets, useTheme
 } from '@fluentui/react';
+import { VscCloud, VscBeaker } from 'react-icons/vsc';
+import { SiWebassembly } from 'react-icons/si';
 import { getContentStyles, getIconButtonStyles } from '~/styles/modal';
 import {RuntimeType} from '~/services/config';
+import './EnvironmentSelectModal.css';
 
 const options = [
   {
     label: 'Go Playground',
     description: 'Build and run code on official Go playground server.',
     type: RuntimeType.GoPlayground,
+    icon: <VscCloud />
   },
   {
     label: 'Go Playground (GoTip)',
     description: 'Run on server with a current unstable development build of Go.',
     type: RuntimeType.GoTipPlayground,
+    icon: <VscBeaker />
   },
   {
     label: 'WebAssembly',
     description: 'Run Go code in your web browser with access to JS API.',
     type: RuntimeType.WebAssembly,
+    icon: <SiWebassembly />
   }
 ]
 
@@ -64,13 +70,18 @@ const EnvironmentSelectModal: React.FC<Props> = ({
       <div className={contentStyles.body}>
         <Stack tokens={stackTokens}>
           {
-            options.map(({label, description, type}) => (
+            options.map(({label, description, type, icon}) => (
               <CompoundButton
                 key={type}
                 secondaryText={description}
                 checked={type === value}
                 className={buttonStyles.button}
                 onClick={() => onClose?.(type)}
+                onRenderIcon={() => (
+                  <div className="EnvironmentSelectButton__icon">
+                    {icon}
+                  </div>
+                )}
               >
                 {label}
               </CompoundButton>
