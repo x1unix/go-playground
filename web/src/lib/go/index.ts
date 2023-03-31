14a3a2a9d161a5de6524467a80b61ac6f13fb314
x1unix <denis0051@gmail.com>
2023-03-31 19:01:55 +0200

    fix: finish binder helper

--- web/src/lib/go/index.ts
+++ web/src/lib/go/index.ts
@@ -1,7 +1,7 @@
 export * from './debug';
 export * from './stack';
 export * from './types';
-export { GoWrapper } from './wrapper/wrapper';
+export { GoWrapper, wrapGlobal } from './wrapper/wrapper';
 export { instantiateStreaming } from './common';
 export type { GoWebAssemblyInstance } from './wrapper/instance';
 
