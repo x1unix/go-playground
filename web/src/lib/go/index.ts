62f3957f6be53f1d1086407b23268400c13f287d
x1unix <denis0051@gmail.com>
2023-04-07 23:25:51 +0200

    feat: export memory view

--- web/src/lib/go/index.ts
+++ web/src/lib/go/index.ts
@@ -1,6 +1,7 @@
 export * from './debug';
 export * from './stack';
 export * from './types';
+export * from './memory';
 export { GoWrapper, wrapGlobal } from './wrapper/wrapper';
 export { instantiateStreaming } from './common';
 export type { GoWebAssemblyInstance } from './wrapper/instance';
