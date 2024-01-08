a365530a9c01d5a77d0099b466f98d459719571a
x1unix <denis0051@gmail.com>
2024-01-08 18:56:12 -0500

    fix: fix imports

--- internal/gorepl/uihost/downloads_js.go
+++ internal/gorepl/uihost/downloads_js.go
@@ -1,4 +1,4 @@
 package uihost
 
-//go:wasmimport gojs github.com/x1unix/go-playground/internal/gowasm/packagedb.onPackageManagerEvent
+//go:wasmimport gojs github.com/x1unix/go-playground/internal/gorepl/uihost.onPackageManagerEvent
 func onPackageManagerEvent(e packageManagerEvent)
