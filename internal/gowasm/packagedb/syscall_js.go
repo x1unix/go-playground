30b52c2159129918aef342ca59af353227e027d5
x1unix <denis0051@gmail.com>
2024-01-08 19:45:31 -0500

    fix: fix wasm imports

--- internal/gowasm/packagedb/syscall_js.go
+++ internal/gowasm/packagedb/syscall_js.go
@@ -1,9 +1,9 @@
 package packagedb
 
-//go:wasmimport gojs github.com/x1unix/go-playground/internal/gowasm/packagedb.registerPackage
+//go:wasmimport gojs github.com/x1unix/go-playground/internal/gowasm/packagedb.lookupPackage
 func lookupPackage(pkgName string, out *[]byte, cb int)
 
-//go:wasmimport gojs github.com/x1unix/go-playground/internal/gowasm/packagedb.lookupPackage
+//go:wasmimport gojs github.com/x1unix/go-playground/internal/gowasm/packagedb.registerPackage
 func registerPackage(pkgName, version string, cb int)
 
 //go:wasmimport gojs github.com/x1unix/go-playground/internal/gowasm/packagedb.removePackage
