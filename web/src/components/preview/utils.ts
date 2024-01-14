00ff8499726e2b0860cf1c8f5b15603840373fa0
x1unix <denis0051@gmail.com>
2024-01-14 18:36:24 -0500

    feat: convert playground image outputs

--- web/src/components/preview/utils.ts
+++ web/src/components/preview/utils.ts
@@ -1,19 +1,39 @@
-import { EvalEvent } from '~/services/api';
+import type { EvalEvent } from '~/services/api';
+
+const imageSectionPrefix = 'IMAGE:';
+const imgRegEx = /^IMAGE:([A-Za-z0-9=+/]+)$/;
 
 export enum Colors {
   Red = '\x1b[31m',
   Reset = '\x1b[0m'
 }
 
+const ESC = '\u001B[';
+const OSC = '\u001B]';
+const BEL = '\u0007';
+
+/**
+ * Formats output event from program.
+ */
 export const formatEvalEvent = ({Message: msg, Kind: type}: EvalEvent) => {
-  if (type === 'stdout') {
-    return msg
+  if (type === 'stderr') {
+    return `${Colors.Red}${msg}${Colors.Reset}`;
   }
 
-  return `${Colors.Red}${msg}${Colors.Reset}`;
+  // Convert Go-playground inline images to iTerm2 inline images protocol.
+  // See:
+  // - https://iterm2.com/documentation-images.html
+  // - https://github.com/sindresorhus/ansi-escapes/blob/main/index.js
+  if (msg.startsWith(imageSectionPrefix) && imgRegEx.test(msg)) {
+    const base64 = msg.slice(imageSectionPrefix.length).trim();
+    //return `\033]1337;File=;inline=1:${base64}\a\n`;
+    return `${OSC}1337;File=inline=1:${base64}${BEL}\n`;
+  }
+
+  return msg;
 }
 
-export const createDebouncableResizeObserver = (callback: () => void, delay: number) => {
+export const createDebounceResizeObserver = (callback: () => void, delay: number) => {
   let timeoutId: NodeJS.Timeout | null = null;
 
   return new ResizeObserver(() => {
