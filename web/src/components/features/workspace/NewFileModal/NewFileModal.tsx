e79b1a4bd1da1fcf58d9faa72da11324edb3007b
x1unix <denis0051@gmail.com>
2024-01-24 02:45:21 -0500

    feat: add new file modal

---
+++ web/src/components/features/workspace/NewFileModal/NewFileModal.tsx
@@ -0,0 +1,114 @@
+import React, {useEffect, useState} from 'react';
+import {
+  Stack,
+  TextField,
+  DefaultButton,
+  PrimaryButton,
+  DefaultSpacing,
+  type IStackTokens
+} from '@fluentui/react';
+
+import { Dialog } from '~/components/elements/modals/Dialog';
+import { DialogActions } from '~/components/elements/modals/DialogActions';
+
+interface Props {
+  isOpen: boolean
+  fileNameValidator?: (fileName?: string) => string|undefined
+  onClose: (fileName?: string) => void
+}
+
+const fileNameRegex = /^(?!.*(\.\.|\/\.|\.\/|\/$))[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\.go$/i;
+const maxFileNameLength = 255;
+
+const verticalStackTokens: IStackTokens = {
+  childrenGap: DefaultSpacing.s1,
+};
+
+const validateFileName = (value?: string, validatorFn?: Props['fileNameValidator']): string|undefined => {
+  if (!value?.length) {
+    return 'File name is required';
+  }
+  if (value.length >= maxFileNameLength) {
+    return 'File name is too long';
+  }
+
+  if (!fileNameRegex.test(value)) {
+    return 'Invalid file name';
+  }
+
+  return validatorFn?.(value);
+}
+
+export const NewFileModal: React.FC<Props> = ({ isOpen, onClose, fileNameValidator }) => {
+  const [fileName, setFileName] = useState<string|undefined>();
+  const [isDirty, setIsDirty] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string|undefined>(undefined);
+
+  const isValid = !errorMessage?.length;
+
+  useEffect(() => {
+    if (isOpen) {
+      return;
+    }
+
+    setFileName('');
+    setIsDirty(false);
+    setErrorMessage('Empty file name');
+  }, [isOpen]);
+
+  useEffect(() => {
+    const errMsg = validateFileName(fileName, fileNameValidator);
+    setErrorMessage(errMsg);
+  }, [fileName, fileNameValidator]);
+
+  return (
+    <Dialog
+      label='New file'
+      isOpen={isOpen}
+      onDismiss={() => onClose()}
+    >
+      <Stack tokens={verticalStackTokens}>
+        <Stack.Item>
+          <span>Enter new file name:</span>
+        </Stack.Item>
+        <Stack.Item>
+          <TextField
+            autoFocus
+            minLength={4}
+            maxLength={maxFileNameLength}
+            defaultValue={fileName}
+            deferredValidationTime={0}
+            placeholder='Example: main.go'
+            onChange={(_, value) => setFileName(value)}
+            onKeyDown={e => {
+              if (e.key === 'Enter' && isValid) {
+                onClose(fileName);
+              }
+            }}
+            onGetErrorMessage={value => {
+              if (!isDirty) {
+                return undefined;
+              }
+
+              setIsDirty(true);
+              return errorMessage;
+            }}
+          />
+        </Stack.Item>
+        <Stack.Item>
+          <DialogActions>
+            <DefaultButton
+              text='Cancel'
+              onClick={() => onClose()}
+            />
+            <PrimaryButton
+              text='OK'
+              onClick={() => onClose(fileName)}
+              disabled={!isValid}
+            />
+          </DialogActions>
+        </Stack.Item>
+      </Stack>
+    </Dialog>
+  );
+}
--- web/src/components/features/workspace/Workspace/Workspace.tsx
+++ web/src/components/features/workspace/Workspace/Workspace.tsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useCallback, useEffect, useMemo } from 'react';
 
 import { TabView } from '~/components/elements/tabs/TabView';
 import type {TabBarAction, TabInfo} from '~/components/elements/tabs/types';
 
 import { CodeEditor } from '../CodeEditor';
 import { FlexContainer } from '../FlexContainer';
+import { NewFileModal } from '../NewFileModal';
 import { skipIndex } from './utils';
 
 
@@ -13,10 +14,11 @@ interface Props {}
 const mockTabName = i => `main${i}.go`
 
 export const Workspace: React.FC<Props> = () => {
+  const [ modalOpen, setModalOpen ] = useState(false);
   const [selectedTab, setSelectedTab] = useState<string>();
   const [tabs, setTabs] = useState<TabInfo[]>([]);
 
-  const onClose = (key: string, i: number) => {
+  const onTabClose = (key: string, i: number) => {
     if (key === selectedTab) {
       const nextId = i === 0 ? 1 : i - 1;
       const nextKey = tabs[nextId]?.key;
@@ -30,15 +32,7 @@ export const Workspace: React.FC<Props> = () => {
       label: 'New file',
       icon: { iconName: 'Add' },
       onClick: () => {
-        const newTab = mockTabName(Date.now());
-        setTabs((prev) => ([
-          ...prev,
-          {
-            key: newTab,
-            label: newTab,
-          }
-        ]));
-        setSelectedTab(newTab);
+        setModalOpen(true);
       }
     },
     {
@@ -46,7 +40,22 @@ export const Workspace: React.FC<Props> = () => {
       icon: { iconName: 'Upload' },
       onClick: () => console.log('upload'),
     }
-  ], [setTabs, setSelectedTab]);
+  ], [setModalOpen]);
+
+  const onModalClose = (fileName?: string) => {
+    setModalOpen(false);
+    if (!fileName) {
+      return;
+    }
+    setTabs((prev) => ([
+      ...prev,
+      {
+        key: fileName,
+        label: fileName,
+      }
+    ]));
+    setSelectedTab(fileName);
+  };
 
   return (
     <TabView
@@ -54,12 +63,16 @@ export const Workspace: React.FC<Props> = () => {
       actions={actions}
       tabs={tabs}
       selectedTab={selectedTab}
-      onClosed={onClose}
+      onClosed={onTabClose}
       onSelected={setSelectedTab}
     >
       <FlexContainer>
         <CodeEditor />
       </FlexContainer>
+      <NewFileModal
+        isOpen={modalOpen}
+        onClose={onModalClose}
+      />
     </TabView>
   )
 };
