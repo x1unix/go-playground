56d9f451820c8b993177980001d552cf8456dc23
x1unix <denis0051@gmail.com>
2024-01-29 19:51:25 -0500

    feat: attach state to workspace

--- web/src/store/notifications/reducers.ts
+++ web/src/store/notifications/reducers.ts
@@ -1,7 +1,7 @@
-import { mapByAction } from "../helpers";
-import { Action } from "../actions";
-import {Notification, NotificationsState} from "./state";
-import { ActionType } from "./actions";
+import { mapByAction } from '../helpers';
+import { Action } from '../actions';
+import {Notification, NotificationsState} from './state';
+import { ActionType } from './actions';
 
 const reducers = mapByAction<NotificationsState>({
   [ActionType.NEW_NOTIFICATION]: (s: NotificationsState, {payload}: Action<Notification>) => (
