65debac7cf21904b34854b7d1899ce3f918e13a9
x1unix <denis0051@gmail.com>
2024-01-31 21:24:03 -0500

    fix: fix snippet url

--- web/src/services/examples/client.ts
+++ web/src/services/examples/client.ts
@@ -1,12 +1,12 @@
 import type { SnippetSource } from './types'
 
 const baseUrl = new URL(import.meta.env.BASE_URL, location.origin)
-const snippetsBaseUrl = new URL('examples', baseUrl)
+const snippetsBaseDir = 'examples'
 
 export const getSnippetFromSource = async (source: SnippetSource): Promise<Record<string, string>> => {
   const { basePath, files } = source
   const promises = files.map(async (file) => {
-    const url = new URL(`${basePath}/${file}`, snippetsBaseUrl)
+    const url = new URL(`${snippetsBaseDir}/${basePath}/${file}`, baseUrl)
     const rsp = await fetch(url)
     if (!rsp.ok) throw new Error(`HTTP Error: ${rsp.status} ${rsp.statusText}`)
     return [file, await rsp.text()] as const
