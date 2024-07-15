e19fb988425c65faa99e51fae60676dd9baeac5d
x1unix <denis0051@gmail.com>
2024-07-15 08:30:55 -0400

    feat: remove service worker

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -19,9 +19,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register({
-  onUpdate: (registration) => {
-    registration.update()
-      .catch(err => console.error('Failed to check updates: ', err))
-  }
-});
+serviceWorkerRegistration.unregister()
