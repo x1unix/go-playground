1b060ab19e1a9507832e94549d43a937c8d98ebf
x1unix <denis0051@gmail.com>
2024-07-18 20:33:08 -0400

    chore: add removed field to state

--- web/src/store/notifications/state.ts
+++ web/src/store/notifications/state.ts
@@ -6,20 +6,92 @@ export enum NotificationType {
 }
 
 export interface Notification {
+  /**
+   * Unique notification ID.
+   *
+   * Used to update or delete notifications.
+   */
   id: string
+
+  /**
+   * Notification severity.
+   *
+   * Info by default.
+   */
   type?: NotificationType
+
+  /**
+   * Notification popup title.
+   */
   title: string
+
+  /**
+   * Popup contents.
+   */
   description?: string
+
+  /**
+   * Controls whether notification can be removed.
+   *
+   * By default is true.
+   */
   canDismiss?: boolean
+
+  /**
+   * Whether notification was dismissed and sheduled for removal.
+   *
+   * Used to play notification hide animation before removing an element from state.
+   */
+  removed?: boolean
+
+  /**
+   * Progress bar information.
+   */
   progress?: {
+    /**
+     * Identifies that current progress can't be determined but some activity is actually running.
+     */
     indeterminate?: boolean
+
+    /**
+     * Total number of points to finish the operation.
+     *
+     * Used as a total value to calculate percentage.
+     *
+     * For example: total number of bytes to download.
+     */
     total?: number
+
+    /**
+     * Current number of points processed.
+     *
+     * For example: current percentage or downloaded bytes count.
+     */
     current?: number
   }
+
+  /**
+   * List of action buttons to show in a pop-up.
+   */
   actions?: Array<{
+    /**
+     * Button label.
+     */
     label: string
+
+    /**
+     * Button unique key.
+     */
     key: string
+
+    /**
+     * Whether action is primary.
+     */
     primary?: boolean
+
+    /**
+     * Click event handler.
+     */
     onClick?: () => void
   }>
 }
