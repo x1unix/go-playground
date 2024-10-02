1fb0bc5c0b2467fa887998fe5d491650166803a4
x1unix <denis0051@gmail.com>
2024-10-02 17:56:25 -0400

    chore: make monaco-editor work in vitest

--- web/src/setupTests.ts
+++ web/src/setupTests.ts
@@ -1,5 +1,17 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom/vitest'
+import { vi } from 'vitest'
+
+// Dependency for monaco-editor tests.
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+})
