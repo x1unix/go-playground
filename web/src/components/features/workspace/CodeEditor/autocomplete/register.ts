7875d91dcb55012d07da57b429395b1b740270bc
Denys Sedchenko <denis0051@gmail.com>
2024-10-06 03:29:05 -0400

    feat: basic Go.Mod syntax support (#430)
    * feat: basic Go.Mod syntax support
    
    * chore: update changelog

--- web/src/components/features/workspace/CodeEditor/autocomplete/register.ts
+++ web/src/components/features/workspace/CodeEditor/autocomplete/register.ts
@@ -3,11 +3,10 @@ import * as monaco from 'monaco-editor'
 import { GoSymbolsCompletionItemProvider } from './symbols'
 import { GoImportsCompletionProvider } from './imports'
 import { GoHoverProvider } from './hover'
+import { LanguageID } from '../grammar'
 import type { StateDispatch } from '~/store'
 import type { DocumentMetadataCache } from './cache'
 import type { LanguageWorker } from '~/workers/language'
-
-const LANG_GO = 'go'
 
 /**
  * Registers all Go autocomplete providers for Monaco editor.
@@ -19,13 +18,13 @@ export const registerGoLanguageProviders = (
 ) => {
   return [
     monaco.languages.registerCompletionItemProvider(
-      LANG_GO,
+      LanguageID.Go,
       new GoSymbolsCompletionItemProvider(dispatcher, cache, langWorker),
     ),
     monaco.languages.registerCompletionItemProvider(
-      LANG_GO,
+      LanguageID.Go,
       new GoImportsCompletionProvider(dispatcher, cache, langWorker),
     ),
-    monaco.languages.registerHoverProvider(LANG_GO, new GoHoverProvider(langWorker, cache)),
+    monaco.languages.registerHoverProvider(LanguageID.Go, new GoHoverProvider(langWorker, cache)),
   ]
 }
