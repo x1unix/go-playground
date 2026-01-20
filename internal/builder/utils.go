f9b488a32c69ea8c533b8c880cb8c057b16f3bb6
x1unix <9203548+x1unix@users.noreply.github.com>
2026-01-19 22:15:59 -0500

    fix: handle suffix in Go version output

--- internal/builder/utils.go
+++ internal/builder/utils.go
@@ -1,11 +1,29 @@
 package builder
 
 import (
+	"regexp"
 	"runtime"
-	"strings"
 )
 
-var goVersion = strings.TrimPrefix(runtime.Version(), "go")
+var (
+	goVersion      string
+	goVersionRegEx = regexp.MustCompile(`(?m)^go (\d+\.\d+\.\d+){1}`)
+)
+
+func init() {
+	goVersion = parseGoVersion(runtime.Version())
+	if goVersion == "" {
+		goVersion = "1.25"
+	}
+}
+
+func parseGoVersion(input string) string {
+	matches := goVersionRegEx.FindStringSubmatch(input)
+	if len(matches) > 1 {
+		return matches[1]
+	}
+	return ""
+}
 
 func generateGoMod(modName string) []byte {
 	return []byte("module " + modName + "\ngo " + goVersion)
