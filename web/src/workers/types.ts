e61b3586e443eb2ef57fb360bdb4bde715782bb3
x1unix <9203548+x1unix@users.noreply.github.com>
2026-03-25 22:51:24 -0400

    feat: add worker ref helper
    Signed-off-by: x1unix <9203548+x1unix@users.noreply.github.com>

--- web/src/workers/types.ts
+++ web/src/workers/types.ts
@@ -1,3 +1,57 @@
+import * as Comlink from 'comlink'
+
 export interface Disposable {
   dispose(): void
+}
+
+export class WorkerRef<T> {
+  private proxy: Comlink.Remote<T> | null = null
+  private worker: Worker | null = null
+  private references = 0
+  private disposePending = false
+
+  constructor(private factory: () => Worker) {}
+
+  private getRemote(): Comlink.Remote<T> {
+    if (this.proxy && this.worker) {
+      return this.proxy
+    }
+
+    const worker = this.factory()
+    this.proxy = Comlink.wrap<T>(worker)
+    this.worker = worker
+    return this.proxy
+  }
+
+  private releaseRemote() {
+    this.proxy?.[Comlink.releaseProxy]()
+    this.worker?.terminate()
+    this.proxy = null
+    this.worker = null
+  }
+
+  async acquire<TResult>(callback: (worker: Comlink.Remote<T>) => Promise<TResult> | TResult): Promise<TResult> {
+    this.references += 1
+
+    try {
+      const worker = this.getRemote()
+      return await callback(worker)
+    } finally {
+      this.references -= 1
+
+      if (this.disposePending && this.references === 0) {
+        this.releaseRemote()
+        this.disposePending = false
+      }
+    }
+  }
+
+  dispose() {
+    this.disposePending = true
+
+    if (this.references === 0) {
+      this.releaseRemote()
+      this.disposePending = false
+    }
+  }
 }
